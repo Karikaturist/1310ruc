0b19b7d9f2b68ac0e6f2f6735f01d22f2ddfcb32
Karikaturist <val_minkov@abv.bg>
2013-10-31 19:50:41 +0200

    Homework 4

---
+++ Program41.cs
@@ -0,0 +1,24 @@
+﻿/*1.	Да се напише програма, която намира сумата на елементите на едномерен масив*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4._1
+{
+    class Program41
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] x = new int [] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int sum=0;
+            for (i = 0; i < 8; i++)
+            {
+                sum += x[i];
+            }
+            Console.WriteLine("Сумата от елементите на масива е: "+sum );
+        }
+    }
+}
---
+++ Program43.cs
@@ -0,0 +1,33 @@
+﻿/*3.	Да се напише програма, която сортира елементите на масив във възходящ ред.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4._3
+{
+    class Program43
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 6, 9, 4, 3, 5, 1, 42, -2 };
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                for (int j = 0; j < x.Length - 1; j++)
+                {
+                    if (x[j] > x[j + 1]) // swap the elements
+                    {
+                        int buffer = x[j];
+                        x[j] = x[j + 1];
+                        x[j + 1] = buffer;
+                    }
+                }
+            }
+            for (int i = 0; i < x.Length; i++) // print the elements
+            {
+                Console.Write(x[i] + " ");
+            }
+        }
+    }
+}
