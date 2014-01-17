de5e1a99c7b9123723e37ffcb6209227983a0415
Karikaturist <val_minkov@abv.bg>
2014-01-17 19:21:28 +0200

    Homework9 (not full)

---
+++ Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{        
+    public class Student
+    {
+        private string name = null;
+        private string surname = null;
+        private string thirdname = null;
+        private readonly string id;
+        private readonly string FakNom;
+        int studentcounter;
+
+
+        public Student(string id, string FakNom)
+    {
+        // TODO: Complete member initialization 
+            this.id = id;
+            this.FakNom = FakNom;
+            this.name = name;
+            this.surname = surname;
+            this.thirdname = thirdname;
+            FakNom = GenerateFakNom();  
+            int ocenka1=2; 
+            int ocenka2=2;
+            this.studentcounter = studentcounter+1;
+    }
+
+         public string Id
+         {
+             get { return id; }
+         }
+
+         private string GenerateFakNom()
+         {
+             return null;
+         }
+        private void AddInfo()
+        {
+        }
+        private void Setid(string id)
+        {
+            //this method validates all the numbers of the ID
+            //and sets the ID if they are valid
+            //else it throws an exception
+        }
+
+        class Student
+        {
+            static void Main(string[] args)
+            {
+                Student student=new Student("id", "FakNom");
+                //string Id;
+                //string GenerateFakNom;
+                //string name = Console.ReadLine();
+                //string surname = Console.ReadLine();
+                //string thirdname = Console.ReadLine();
+
+        }
+
+      
+    
+    }
+    }
+}
